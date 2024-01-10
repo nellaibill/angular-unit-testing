fefd5aa992be6d4f0966ff18bf313bb46706787f
Saleem <nellaibill@gmail.com>
2024-01-10 22:57:35 +0530

    Added : Spyon,callthrough,createSpyObject

--- src/app/calc.service.spec.ts
+++ src/app/calc.service.spec.ts
@@ -1,9 +1,14 @@
-import { CalcService } from "./calc.service"
+import { CalcService } from './calc.service';
+import { SharedService } from './shared.service';
 
-describe("CalcService",()=>{
-  it("should multiply two numbers",()=>{
-    const calc = new CalcService();
-    const result =calc.multiply(3,5);
+describe('CalcService', () => {
+  it('should multiply two numbers', () => {
+    //const shared = new SharedService();
+    //spyOn(shared,"mySharedFunction").and.callThrough();
+    const shared = jasmine.createSpyObj('SharedService', ['mySharedFunction']);
+    const calc = new CalcService(shared);
+    const result = calc.multiply(3, 5);
     expect(result).toBe(15);
-  })
-})
+    expect(shared.mySharedFunction).toHaveBeenCalled();
+  });
+});
