f26f85e48358bb12ce729bfa91606873113ab6bc
Saleem <nellaibill@gmail.com>
2024-01-10 08:57:19 +0530

    Initial-Checkin-BasicComponent-Service-describe,it

---
+++ src/app/calc.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CalcService {
+  constructor() {}
+  multiply(a: number, b: number) {
+    return a * b;
+  }
+}
